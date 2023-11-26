6b106fb4ca57e1e38f79113566c2e110d0a2b5aa
KagErou710 <56529285+KagErou710@users.noreply.github.com>
2023-11-26 22:55:35 +0700

    added many part
    Front End, Back End progress
    - can connect DB and get data (lng, lat, severity).
    - show data from DB.
    - design for every page, just waiting what data needed to show in statistic part
    - searching place by name
    Updates
    -  change color of circle according to severity

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 from flask import Flask, render_template
-# import sqlite3 
+import sqlite3 
+
+# SQLiteデータベースに接続
+conn = sqlite3.connect('ml_db.db')
+cursor = conn.cursor()
+# cursor.execute('CREATE TABLE Accidents(a_id INTEGER PRIMARY KEY, a_lat FLOAT NOT NULL, a_lng FLOAT NOT NULL,severity FLOAT NOT NULL);')
+# cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (1, 13.75 , 100.53125, 20);')
+# cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (2, 13.76 , 100.54, 30);')
+# cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (3, 13.77 , 100.55, 40);')
+# cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (4, 13.78 , 100.56, 50);')
+# cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (5, 13.79 , 100.57, 60);')
+cursor.execute('SELECT a_lng, a_lat FROM Accidents')
+coordinates = cursor.fetchall()
+conn.close()
+for lng, lat in coordinates:
+    print(f'Longitude: {lng}, Latitude: {lat}')
+
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+def predict_accidents():
+    # SQLiteデータベースに接続
+    conn = sqlite3.connect('ml_db.db')
+    cursor = conn.cursor()
+    # cursor.execute('CREATE TABLE Accidents(a_id INTEGER PRIMARY KEY, a_lat FLOAT NOT NULL, a_lng FLOAT NOT NULL,severity FLOAT NOT NULL);')
+    # cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (1, 13.75 , 100.53125, 20);')
+    # cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (2, 13.76 , 100.54, 30);')
+    # cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (3, 13.77 , 100.55, 40);')
+    # cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (4, 13.78 , 100.56, 50);')
+    # cursor.execute('INSERT INTO Accidents (a_id, a_lat, a_lng, severity) VALUES (5, 13.79 , 100.57, 60);')
+    cursor.execute('SELECT a_lng, a_lat, severity FROM Accidents')
+    coordinates = cursor.fetchall()
+    conn.close()
+    # for lng, lat in coordinates:
+    #     print(f'Longitude: {lng}, Latitude: {lat}')
 
-# # SQLiteデータベースに接続
-# conn = sqlite3.connect('your_database.db')
-# cursor = conn.cursor()
-
-
-# # データベースから座標を取得するエンドポイント
-# @app.route('/get_coordinates', methods=['GET'])
-# def get_coordinates():
-#     conn = sqlite3.connect('your_database.db')
-#     cursor = conn.cursor()
-#     cursor.execute('SELECT lng, lat FROM coordinates')
-#     coordinates = cursor.fetchall()
-#     conn.close()
-#     return jsonify({'coordinates': coordinates})
+    return coordinates
 
 @app.route('/')
 def home():
@@ -30,7 +49,9 @@ def index():
 
 @app.route('/predict')
 def predict_page():
-    return render_template('predict.html')
+    data = predict_accidents()
+    print(data)
+    return render_template('predict.html', predicts = data)
 
 @app.route('/statistics')
 def statistics_page():
